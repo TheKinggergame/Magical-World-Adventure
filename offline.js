99eeee6d365d3a555f8187da62ed20726bbe49df
TheKinggergame <30756695+TheKinggergame@users.noreply.github.com>
2019-06-23 18:33:56 +0700

    Magical World Adventure

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1561279076,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ground-sheet0.png",
+		"images/player-sheet0.png",
+		"images/fireball-sheet0.png",
+		"images/fireball-sheet1.png",
+		"images/f9633343da0449c1aa5547a950b-sheet0.png",
+		"images/heart-sheet0.png",
+		"images/tower-sheet0.png",
+		"images/book-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/preview-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/untitled-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/55b3dcec443a8a6d5f4251f0ef0ccb-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
